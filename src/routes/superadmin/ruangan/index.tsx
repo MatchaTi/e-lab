824a2e49d5bee1550d533af70870f86a5c2a9bf6
MatchaTi <tehmatcha28@gmail.com>
2025-05-30 16:46:36 +0800

    chore(ruangan): WIP list rooms

--- src/routes/superadmin/ruangan/index.tsx
+++ src/routes/superadmin/ruangan/index.tsx
@@ -1,10 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router';
 import StaffLayout from '@/components/StaffLayout';
+import MasterDataHeader from '@/components/MasterDataHeader';
 
 export const Route = createFileRoute('/superadmin/ruangan/')({
     component: RouteComponent,
 });
 
 function RouteComponent() {
-    return <StaffLayout>Hello Ruangan</StaffLayout>;
+    return (
+        <StaffLayout>
+            <main className='p-6 space-y-6 rounded-lg shadow-lg bg-base-200 shadow-base-content/5'>
+                <MasterDataHeader
+                    title='List Ruangan'
+                    description='Halaman untuk menampilkan list ruangan pada sistem.'
+                    createHref='/superadmin/ruangan/tambah-ruangan'
+                />
+                {/* TODO: Change this to a table component MasterDataTable*/}
+                <div>Table</div>
+            </main>
+        </StaffLayout>
+    );
 }
