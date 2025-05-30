825cc9d17d6b481ffe096936d38cd56bef9af997
MatchaTi <tehmatcha28@gmail.com>
2025-05-30 17:31:30 +0800

    feat(ruangan): WIP detail ruangan

---
+++ src/routes/superadmin/ruangan/detail-ruangan.$id.tsx
@@ -0,0 +1,66 @@
+import { Icon } from '@iconify/react/dist/iconify.js';
+import { createFileRoute } from '@tanstack/react-router';
+import StaffLayout from '@/components/StaffLayout';
+import Fieldset from '@/components/Fieldset';
+
+export const Route = createFileRoute('/superadmin/ruangan/detail-ruangan/$id')({
+    component: RouteComponent,
+});
+
+function RouteComponent() {
+    const { id } = Route.useParams();
+    console.log('Detail room with ID:', id);
+
+    // TODO: Fetch existing room data using the ID and populate the form fields
+    // TODO: Handle form submission to update the room data
+
+    return (
+        <StaffLayout>
+            <main className='p-6 space-y-6 rounded-lg shadow-lg bg-base-200 shadow-base-content/5'>
+                <h1 className='text-3xl font-bold'>Detail Data Ruangan</h1>
+                <p>Halaman untuk menampilkan data ruangan pada sistem.</p>
+
+                <section className='grid gap-6 lg:grid-cols-2'>
+                    <Fieldset className='w-full'>
+                        <Fieldset.Legend>Nama Ruangan</Fieldset.Legend>
+                        <Fieldset.Body className='w-full input'>
+                            <Icon icon='mingcute:school-line' />
+                            <span>Nama Ruangan</span>
+                        </Fieldset.Body>
+                    </Fieldset>
+
+                    <Fieldset className='w-full'>
+                        <Fieldset.Legend>Kode Ruangan</Fieldset.Legend>
+                        <Fieldset.Body className='w-full input'>
+                            <Icon icon='tabler:hash' />
+                            <span>Kode Ruangan</span>
+                        </Fieldset.Body>
+                    </Fieldset>
+
+                    <Fieldset className='w-full'>
+                        <Fieldset.Legend>Kapasitas Ruangan</Fieldset.Legend>
+                        <Fieldset.Body className='w-full input'>
+                            <Icon icon='ix:capacity' />
+                            <span>Kapasitas Ruangan</span>
+                        </Fieldset.Body>
+                    </Fieldset>
+
+                    <Fieldset className='w-full'>
+                        <Fieldset.Legend>Preview Ruangan</Fieldset.Legend>
+                        <Fieldset.Body className='w-full input'>
+                            <Icon icon='iconoir:eye' />
+                            <span>Preview Ruangan</span>
+                        </Fieldset.Body>
+                    </Fieldset>
+
+                    <Fieldset className='w-full lg:col-span-2'>
+                        <Fieldset.Legend>Preview Ruangan</Fieldset.Legend>
+                        <Fieldset.Body className='w-full textarea field-sizing-content'>
+                            <span>Preview Ruangan</span>
+                        </Fieldset.Body>
+                    </Fieldset>
+                </section>
+            </main>
+        </StaffLayout>
+    );
+}
