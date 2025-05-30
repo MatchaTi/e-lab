cf3d6f97a57241ac00e9c7d6450b1e0b59e473cc
MatchaTi <tehmatcha28@gmail.com>
2025-05-30 17:05:29 +0800

    chore: delete unecesarry

--- src/routes/superadmin/ruangan/update-ruangan.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/superadmin/ruangan/update-ruangan')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/superadmin/ruangan/update-ruangan"!</div>
-}
